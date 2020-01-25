da9186695ab7fd390724b1e607cc14bfd82378c1
Rob Hardt <rhardt@pivotal.io>
2020-01-24 16:53:46 -0800

    turned alpine stable into k8s echoserver

---
+++ stable/alpine/run.sh
@@ -0,0 +1,10 @@
+echo "Generating self-signed cert"
+mkdir -p /certs
+chown -R 1001:1001 /certs
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 \
+-keyout /certs/privateKey.key \
+-out /certs/certificate.crt \
+-subj "/C=UK/ST=Warwickshire/L=Leamington/O=OrgName/OU=IT Department/CN=example.com"
+
+echo "Starting nginx"
+nginx -g "daemon off;"
